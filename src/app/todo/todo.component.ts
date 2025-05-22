bd55824524c65d9f4acd2fb67f9e8b9e7046d3b9
iori-sutani <io.sutani@gmail.som>
2025-05-22 19:34:15 +0900

    一括削除ボタンを作成

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -14,7 +14,7 @@ export class TodoComponent {
   newTodo: string = '';
 
   addTodo() {
-    const trimmed = this.newTodo.trim();
+    const trimmed = this.newTodo.trim();   // 空白を除去する
     if (trimmed) {
       this.todos.push({ text: trimmed, completed: false });     //Todoのリストをここで管理する
       this.newTodo = '';     //入力中のTodoを一時的に保持する変数
@@ -27,5 +27,13 @@ export class TodoComponent {
 
   toggleComplete(index: number) {
     this.todos[index].completed = !this.todos[index].completed;
+  }
+
+  clearCompleted() {
+    this.todos = this.todos.filter(todos => !todos.completed); //初期値はfalseだから!をつけてtrueにする->trueのものを削除するということ
+  }
+
+  hasCompletedTodos(): boolean {
+    return this.todos.some(todo => todo.completed); //completedがtrueのものがあるかどうか
   }
 }
